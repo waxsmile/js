6feb679ef4fa917fd31989c2d385286e41153775
Daniil <test@test.ru>
2018-07-11 16:19:21 +0300

    Это третий коммит

--- OptimizedHTML-4-master/app/js/common.js
+++ OptimizedHTML-4-master/app/js/common.js
@@ -1,7 +1,8 @@
-$(function() {
+
 
 let yourMoney = prompt("Ваш бюджет?");
 let yourName = prompt("Название вашего магазина?");
+let time = 19;
 
 let mainList ={
   budget: yourMoney,
@@ -13,11 +14,26 @@ let mainList ={
 
 for (let i = 0; i < 5; i++){
   let a = prompt("Какой тип товаров будем продавать?");
-  mainList.shopGoods[i] = a;
+
+  if ((typeof(a)) === 'string' && (typeof(a)) === null && a !='' && a.lenght <50 ){
+	  console.log('Всё верно');
+	  mainList.shopGoods[i] =a; 
+  }else{
+
+  }
 }
+
+if (time < 0){
+	console.log('Такого не может быть!');
+} else if(time > 8 && time <20){
+	console.log('Время работать!');
+	}else if(time < 24){
+		console.log('Слишком поздно!');
+		}else {
+			console.log('В сутка только 24 часа');
+		};
 
 alert(mainList.budget / 30);
 
 console.log(mainList);
 
-});
